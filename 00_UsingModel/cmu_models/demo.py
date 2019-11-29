c7620e246096a3397fd53aaa66a5f4cdbc0bbd02
Kinda <lamkinun@gmail.com>
2019-11-29 11:45:52 +0800

    pose

--- 00_UsingModel/cmu_models/demo.py
+++ 00_UsingModel/cmu_models/demo.py
@@ -1,11 +1,9 @@
 from OpenPose import OpenPose
 import cv2
-import keras
 import numpy as np
 
 
 pose = OpenPose()
-model = keras.models.load_model("model.h5")
 
 camera = cv2.VideoCapture(0)
 while camera.isOpened():
@@ -13,30 +11,13 @@ while camera.isOpened():
     if not success:
         continue
 
-    # frame = cv2.imread("sample1.jpg", cv2.IMREAD_COLOR)
-    frame = cv2.resize(frame, (720, 480))
-
-    points = pose.detect(frame)
+    points = pose.detect(frame, in_height=168)
     for p in points:
         pose.draw(frame, p)
 
-        data = []
-        for i in range(len(p)):
-            if i in [1, 8, 9, 10, 11, 12, 13]:
-                data.append(p[i][0])
-                data.append(p[i][1])
-        data = np.array([data])
-        predict = model.predict(data)
-        idx = np.argmax(predict)
-        labels = ['fall', 'sit', 'squat', 'stand']
-        print(idx, labels[idx])
-        # cv2.imshow("frame", frame)
-        # cv2.waitKey(0)
-    print()
-
     cv2.imshow("frame", frame)
 
-    key_code = cv2.waitKey(500)
+    key_code = cv2.waitKey(1)
     if key_code in [27, ord('q')]:
         break
 camera.release()
---
+++ 00_UsingModel/cnn_vgg/demo.py
@@ -0,0 +1,90 @@
+from keras.applications import VGG16
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+
+image_shape = (32, 32, 3)
+
+vgg = VGG16(
+    weights='imagenet',
+    include_top=False,
+    input_shape=image_shape
+)
+for layer in vgg.layers[:-4]:
+    layer.trainable = False
+for layer in vgg.layers:
+    print(layer, layer.trainable)
+
+model = models.Sequential()
+model.add(vgg)
+model.add(layers.Flatten())
+model.add(layers.Dense(units=4, activation="softmax"))
+model.summary()
+
+train_data_gen = ImageDataGenerator(
+    rescale=1.0 / 255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+valid_data_gen = ImageDataGenerator(rescale=1.0 / 255)
+
+train_batch_size = 100
+valid_batch_size = 10
+
+train_generator = train_data_gen.flow_from_directory(
+    "../../../datasets/pcms/1126/",
+    target_size=image_shape[:2],
+    batch_size=train_batch_size,
+    class_mode='categorical',
+    shuffle=True
+)
+
+validation_generator = valid_data_gen.flow_from_directory(
+    "../../../datasets/pcms/photos/",
+    target_size=image_shape[:2],
+    batch_size=valid_batch_size,
+    class_mode='categorical',
+    shuffle=False
+)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=train_generator.samples / train_generator.batch_size,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples / validation_generator.batch_size,
+    verbose=1)
+
+model.save('small_last4.h5')
+print(history.history)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'b', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
