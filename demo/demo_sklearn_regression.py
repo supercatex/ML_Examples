4a43a8294e54a1b595b47547b2116eda1bd8b4db
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2025-03-31 16:00:23 +0800

    Update demo_sklearn_regression.py

--- demo/demo_sklearn_regression.py
+++ demo/demo_sklearn_regression.py
@@ -20,24 +20,34 @@ models = {
     "Polynomial Regression (4)": make_pipeline(PolynomialFeatures(4), LinearRegression()),
     "Polynomial Regression (5)": make_pipeline(PolynomialFeatures(5), LinearRegression()),
     "Polynomial Regression (6)": make_pipeline(PolynomialFeatures(6), LinearRegression()),
+    "Polynomial Regression (7)": make_pipeline(PolynomialFeatures(7), LinearRegression()),
+    "Polynomial Regression (8)": make_pipeline(PolynomialFeatures(8), LinearRegression()),
+    "Polynomial Regression (9)": make_pipeline(PolynomialFeatures(9), LinearRegression()),
+    "Polynomial Regression(10)": make_pipeline(PolynomialFeatures(10), LinearRegression()),
 }
 
+scores = []
 width = 5
-figure = plt.figure(figsize=(width * (len(models) + 1), width))
+plt.figure(figsize=(width * (len(models) + 1) // 2, width * 2))
 i = 0
 for name, model in models.items():
     model.fit(X_train, y_train)
     score = model.score(X_test, y_test)
+    scores.append(score)
     print(name, "score:", score)
 
     i = i + 1
-    ax = plt.subplot(1, len(models) + 1, i)
+    ax = plt.subplot(2, (len(models) + 1) // 2, i)
     ax.scatter(X_train, y_train, color="r")
+    ax.scatter(X_test, y_test, color="y")
     ax.set_title(name)
 
     data = list(np.concatenate((X_train, y_train), axis=1))
     data.sort(key=lambda x: x[0])
     data = np.array(data)
-    plt.plot(data[:, 0], model.predict(data[:, 0].reshape(-1, 1)), color="b")
+    ax.plot(data[:, 0], model.predict(data[:, 0].reshape(-1, 1)), color="b")
+
+plt.figure()
+plt.plot(range(1, len(models) + 1), scores)
 
 plt.show()
